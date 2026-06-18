bfb5a6ecb38a0e93daf8bc5d9999ea38ff78d40e
Dylan Goncalves <dylanphx93@gmail.com>
2026-06-18 03:08:10 +0200

    Create Wallet class

---
+++ src/Level-1/part-2/Wallet.php
@@ -0,0 +1,14 @@
+<?php
+class Wallet {
+  private string $bankCards;
+  private string $cash;
+  private string $idCard;
+  private ?string $transitCard;
+
+  public function __construct(string $bankCards, string $cash, string $idCard, ?string $transitCard) {
+    $this->bankCards = $bankCards;
+    $this->cash = $cash;
+    $this->idCard = $idCard;
+    $this->transitCard = $transitCard;
+  }
+}  
