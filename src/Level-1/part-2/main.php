9cd9341f6599239d608cefafa7de6b04efd3b79a
Dylan Goncalves <dylanphx93@gmail.com>
2026-06-18 11:59:27 +0200

    create the main file and use the classes

---
+++ src/Level-1/part-2/main.php
@@ -0,0 +1,15 @@
+<?php
+require_once "Wallet.php";
+require_once "Keys.php";
+require_once "Smartphone.php";
+require_once "Transport.php";
+require_once "Person.php";
+
+$wallet = new Wallet("Mastercard-Visa", "50€","Y200456879f" ,"T-Mobilitat");
+$keys = new Keys();
+$Smartphone  = new Smartphone("Samsung");
+$transport = new Transport(TransportType::Car);
+$person = new Person ($wallet, $keys, $Smartphone, $transport);
+
+$person->leaveHouse();
+ 
