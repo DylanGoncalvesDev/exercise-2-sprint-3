94b4f6da730dae8a96161cf59e717ae8ca671fe3
Dylan Goncalves <dylanphx93@gmail.com>
2026-07-20 19:33:51 +0200

    refactored code to apply correctly the singleton pattern

--- src/Level-1/part-1/Tigger.php
+++ src/Level-1/part-1/Tigger.php
@@ -4,23 +4,21 @@ class Tigger {
     private static int $counter = 0;
 
    private function __construct() {
-        echo "Building character..." . PHP_EOL;
    }
 
    public static function getInstance(): Tigger {
       if (self::$instance === null) {
           self::$instance = new Tigger();
       }
-
       return self::$instance;
    }
 
-   public static function roar() {
+   public function roar(): string {
         self::$counter++;
-        echo "Grrr!" . PHP_EOL;
+        return "Grrr!" . PHP_EOL;
    }
 
-   public static function getCounter(): string { 
+   public function getCounter(): string { 
       return "Numero de Roars: " . self::$counter;
    }
 }
